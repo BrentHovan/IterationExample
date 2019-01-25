de61bedec146f9e6b7038b17a921fac9b31aa993
Brent Hovan <brenthovan@gmail.com>
2019-01-24 20:08:59 -0500

    Version 1.1

--- IterationExample/Program.cs
+++ IterationExample/Program.cs
@@ -6,7 +6,33 @@ namespace IterationExample
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            try
+            {
+                Console.Write("Enter an integer value between 1 and 100. ");
+                string input;
+                int n;
+                input = Console.ReadLine();
+                n = int.Parse(input);
+
+                if (n > 0 && n < 101)
+                {
+                    for (int i = 1; i < n + 1; i++)
+                    {
+                        Console.WriteLine("You have entered " + n + ". This is the current integer value in the loop: " + i + ".");
+                    }
+                    Console.ReadKey();
+                }
+                else
+                {
+                    Console.Write("Invalid number entered. Your penchant for noncompliance is noted, human.");
+                    Console.ReadKey();
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Invalid input. Your penchant for noncompliance is noted, human.");
+                Console.ReadKey();
+            }
         }
     }
 }
